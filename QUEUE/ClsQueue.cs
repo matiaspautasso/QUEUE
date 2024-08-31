6aecbcc3f974ebe65b4a5532fe2fc9c029ae46cc
Matias Pautasso <matiaspautasso@outlook.com>
2024-08-31 15:06:54 -0300

    Agregar archivos de proyecto.

---
+++ QUEUE/ClsQueue.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUEUE
+{
+    internal class ClsQueue
+    {
+        private ClsNode first;
+        private ClsNode last;
+
+        public ClsNode First
+        {
+            get { return first; }
+            set { first = value; }
+
+        }
+        public ClsNode Last
+        {
+            get { return last; }
+            set { last = value; }
+        }
+        public void iNSERT  (ClsNode New)
+        {
+            if (First == null)
+            {
+                First = New;
+                Last = New;
+            }
+            else
+            {
+                Last.NEXT = New;
+                Last = New;
+
+            }
+        }
+        public void Delete()
+        {
+            if (First == Last)
+            {
+                First = null;
+                Last = null;
+            }
+            else
+            {
+                First = First.NEXT;
+
+            }
+        }
+        public void Navigate(ListBox LIST)
+        {
+            ClsNode aux = First;
+            LIST.Items.Clear();
+            while (aux != null)
+            {
+                LIST.Items.Add(aux.Code);
+                aux = aux.NEXT;
+            }
+        }
+
+        public void Navigate(DataGridView DGV)
+        {
+            ClsNode aux = First;
+            DGV.Rows.Clear();
+
+            while (aux != null)
+            {
+                DGV.Rows.Add(aux.Code, aux.Name, aux.Proccess);
+                aux = aux.NEXT;
+            }
+        }
+        public void Navigate()
+        {
+            ClsNode aux = First;
+            StreamWriter AD = new StreamWriter("Queue.csv", false, Encoding.UTF8);
+            AD.WriteLine("Waiting List\n");
+            AD.WriteLine("Code;Name;Process");
+            while (aux != null)
+            {
+                AD.Write(aux.Code);
+                AD.Write(";");
+                AD.Write(aux.Name);
+                AD.Write(";");
+                AD.WriteLine(aux.Proccess);
+                aux = aux.NEXT;
+
+            }
+            AD.Close();
+        }
+    }
+}
---
+++ QUEUE/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QUEUE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        ClsQueue peopleRow = new ClsQueue();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            ClsNode OBJ = new ClsNode();
+            OBJ.Code = int.Parse(txtCode.Text);
+            OBJ.Name = txtName.Text;
+            OBJ.Proccess = txtProcess.Text;
+
+            peopleRow.iNSERT(OBJ);
+            peopleRow.Navigate(DgvCola);
+            peopleRow.Navigate(lstQueue);
+            peopleRow.Navigate();
+            txtCode.Text = "";
+            txtName.Text = "";
+            txtProcess.Text = "";
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (peopleRow.First != null)
+            {
+                lblCode.Text = peopleRow.First.Code.ToString();
+               
+                lblName.Text = peopleRow.First.Name.ToString();
+                lblProcess.Text = peopleRow.First.Proccess.ToString();
+                peopleRow.Delete();
+                peopleRow.Navigate(DgvCola);
+                peopleRow.Navigate(lstQueue);
+                peopleRow.Navigate();
+
+
+            }
+            else
+            {
+                lblCode.Text = "";
+                lblName.Text = "";
+                lblProcess.Text = "";
+
+            }
+
+        }
+    }
+}
